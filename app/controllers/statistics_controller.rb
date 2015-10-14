6e5d0aefc67f291cdeaaf9e25f4e89e922ee01f1
xavi666 <xavier@flowlens.com>
2015-10-14 08:57:15 +0100

    geting players and teams of season 2014-2015

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -24,80 +24,79 @@ class StatisticsController < ApplicationController
             "RIO NATURA MONBUS OBRADOIRO" => 17, 
             "VALENCIA BASKET CLUB" => 18
           }
+    @partits = []
 
+    cod_edicions = ["59"]
+    cod_edicions.each_with_index {|cod_edicion, index| 
+      (1..2).each do |partido|
+        url_jornada = "http://www.acb.com/stspartido.php?cod_competicion=LACB&cod_edicion=59&partido="+partido.to_s
+        pagina_partit = Nokogiri::HTML(open(url_jornada))
+        # dades jugadors
+        taula_jugadors = pagina_partit.css("table.estadisticasnew")[1]
 
-    [1..2].each do |jornada|
-      puts "-----------> "
-      puts jornada
-    end
-    index_page = doc = Nokogiri::HTML(open("http://www.acb.com/resulcla.php?codigo=LACB-59&jornada=1&resultados="))
+        rows_jugadors = taula_jugadors.search('//tr')
+        @jugadors = rows_jugadors.collect do |row|
+          detail = {}
+          [
+            [:dorsal, 'td[1]/text()'],
+            [:nom, 'td[2]/a/text()'],
+            [:minuts, 'td[3]/text()'],
+            [:punts, 'td[4]/text()'],
+            [:t2, 'td[5]/text()'],
+            [:t2p, 'td[6]/text()'],
+            [:t3, 'td[7]/text()'],
+            [:t3p, 'td[8]/text()'],
+            [:t1, 'td[9]/text()'],
+            [:t1p, 'td[10]/text()'],
+            [:rebots, 'td[11]/text()'],
+            [:rebots_d, 'td[12]/text()'],
+            [:rebots_o, 'td[13]/text()'],
+            [:assistencies, 'td[14]/text()'],
+            [:recuperades, 'td[15]/text()'],
+            [:perdues, 'td[16]/text()'],
+            [:contratacs, 'td[17]/text()'],
+            [:taps_f, 'td[18]/text()'],
+            [:taps_r, 'td[19]/text()'],
+            [:mates, 'td[20]/text()'],
+            [:faltes_f, 'td[21]/text()'],
+            [:faltes_r, 'td[22]/text()'],
+            [:mes_menys, 'td[23]/text()'],
+            [:valoracio, 'td[24]/text()']
+          ].each do |name, xpath|
+            detail[name] = row.at_xpath(xpath).to_s.strip
+          end
+          if is_number?(detail[:dorsal])
+            detail
+          end
+        end
+        @jugadors.delete_if { |k, v| k.nil? }
+        ap @jugadors
 
-    doc = Nokogiri::HTML(open("http://www.acb.com/fichas/LACB60005.php"))
-    #@nodes = doc.search("//table[@class='estadisticasnew']/tr")
+        # equips
+        taula_equips = pagina_partit.css("div.titulopartidonew")[0]
+        row_equips = taula_equips.search('tr')
+        @equips = row_equips.collect do |row|
+          detail = {}
+          [
+            [:local, 'td[1]/text()'],
+            [:visitant, 'td[2]/text()']
+          ].each do |name, xpath|
+            equip = row.at_xpath(xpath).to_s.strip
+            equip.slice!(0) if name == :visitant
+            equip = equip[0..-3] if name == :local
 
-    # dades jugadors
-    taula_jugadors = doc.css("table.estadisticasnew")[1]
+            detail[name] = equip
+          end
+          if detail[:local] != ''
+            detail
+          end
+        end
 
-    rows_jugadors = taula_jugadors.search('//tr')
-    @jugadors = rows_jugadors.collect do |row|
-      detail = {}
-      [
-        [:dorsal, 'td[1]/text()'],
-        [:nom, 'td[2]/a/text()'],
-        [:minuts, 'td[3]/text()'],
-        [:punts, 'td[4]/text()'],
-        [:t2, 'td[5]/text()'],
-        [:t2p, 'td[6]/text()'],
-        [:t3, 'td[7]/text()'],
-        [:t3p, 'td[8]/text()'],
-        [:t1, 'td[9]/text()'],
-        [:t1p, 'td[10]/text()'],
-        [:rebots, 'td[11]/text()'],
-        [:rebots_d, 'td[12]/text()'],
-        [:rebots_o, 'td[13]/text()'],
-        [:assistencies, 'td[14]/text()'],
-        [:recuperades, 'td[15]/text()'],
-        [:perdues, 'td[16]/text()'],
-        [:contratacs, 'td[17]/text()'],
-        [:taps_f, 'td[18]/text()'],
-        [:taps_r, 'td[19]/text()'],
-        [:mates, 'td[20]/text()'],
-        [:faltes_f, 'td[21]/text()'],
-        [:faltes_r, 'td[22]/text()'],
-        [:mes_menys, 'td[23]/text()'],
-        [:valoracio, 'td[24]/text()']
-      ].each do |name, xpath|
-        detail[name] = row.at_xpath(xpath).to_s.strip
+        @equips.delete_if { |k, v| k.nil? }
+        @partits.push(@equips)
+        #ap @equips
       end
-      if is_number?(detail[:dorsal])
-        detail
-      end
-    end
-    @jugadors.delete_if { |k, v| k.nil? }
-    ap @jugadors
-
-    # equips
-    taula_equips = doc.css("div.titulopartidonew")[0]
-    puts taula_equips
-    row_equips = taula_equips.search('tr')
-    puts row_equips
-    @equips = row_equips.collect do |row|
-      detail = {}
-      [
-        [:local, 'td[1]/text()'],
-        [:visitant, 'td[2]/text()']
-      ].each do |name, xpath|
-        detail[name] = row.at_xpath(xpath).to_s.strip
-      end
-      if detail[:local] != ''
-        detail
-      end
-    end
-
-    @equips.delete_if { |k, v| k.nil? }
-    ap @equips
-
-
+    }
   end
 
   def is_number? string
