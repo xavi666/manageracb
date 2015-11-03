8e4884937f1a925847a1d11316944dc68603a460
Xavi <xavier@flowlens.com>
2015-11-03 20:47:23 +0000

    Ai4r

--- app/controllers/predictions_controller.rb
+++ app/controllers/predictions_controller.rb
@@ -2,16 +2,40 @@ class PredictionsController < ApplicationController
 
   require 'rubygems'
   require 'ai4r'
+  require "bundler/setup"
+  Bundler.require
+
+  include Ai4r::Classifiers
+  include Ai4r::Data
 
   def index
     #@predictions = Prediction.all
   end
 
   def create
-    DATA_LABELS = [ 'city', 'age_range', 'gender', 'marketing_target' ]
-
-    #@predictions = Prediction.create(prediction_params)
-    redirect_to action: "index"
+    data_labels = [ 'city', 'age_range', 'gender', 'marketing_target' ]
+    data_items = [  
+           ['New York',  '<30',      'M',  'Y'],
+           ['Chicago',   '<30',      'M',  'Y'],
+           ['Chicago',   '<30',      'F',  'Y'],
+           ['New York',  '<30',      'M',  'Y'],
+           ['New York',  '<30',      'M',  'Y'],
+           ['Chicago',   '[30-50)',  'M',  'Y'],
+           ['New York',  '[30-50)',  'F',  'N'],
+           ['Chicago',   '[30-50)',  'F',  'Y'],
+           ['New York',  '[30-50)',  'F',  'N'],
+           ['Chicago',   '[50-80]',  'M',  'N'],
+           ['New York',  '[50-80]',  'F',  'N'],
+           ['New York',  '[50-80]',  'M',  'N'],
+           ['Chicago',   '[50-80]',  'M',  'N'],
+           ['New York',  '[50-80]',  'F',  'N'],
+           ['Chicago',   '>80',      'F',  'Y']
+         ]
+    data_set = DataSet.new(:data_items => data_items, :data_labels => data_labels)
+    id3 = Ai4r::Classifiers::ID3.new.build(data_set)   
+    var = id3.eval(['New York', '<30', 'M'])
+    puts var
+    #redirect_to action: "index"
   end
 
   def update
