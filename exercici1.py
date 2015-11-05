6694087792660fc9d7f1568da9eb0b02836eedaa
Xavi <xavier@flowlens.com>
2015-11-05 23:53:18 +0000

    python

---
+++ exercici1.py
@@ -0,0 +1,31 @@
+from sklearn import preprocessing
+
+# function declarations
+
+def classify(clf, train, test):
+	trainX = list(map(lambda l: list(map(int, l[2:7])), train))
+	testX = list(map(lambda l: list(map(int, l[2:7])), test))
+	trainY = list(map(lambda l: l[8], train))
+	testY = list(map(lambda l: l[8], test))	
+
+	# Estandaritzacio
+	scaler = preprocessing.StandardScaler().fit(trainX)
+	trainX = scaler.transform(trainX)
+	testX = scaler.transform(testX)
+
+	clf.fit(trainX, trainY)
+
+	predictions = clf.predict(testX)
+	return sum(map(lambda x, y: x == y, predictions, testY)) / len(testX) * 100
+
+def sets_svm_prec(clf, l):
+	# training set: 2 of each 3
+	train = list(map(lambda x: x[1],
+		filter(lambda v: v[0] % 3 != 0, enumerate(l))))
+
+	# test set: 1 of each 3
+	test = list(map(lambda x: x[1],
+		filter(lambda v: v[0] % 3 == 0, enumerate(l))))
+
+	# application of svm
+	return classify(clf, train, test)
