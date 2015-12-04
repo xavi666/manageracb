26f15f867a0793c999fa44fd1a73d824e14b60ae
xavi666 <xavier@flowlens.com>
2015-12-04 13:31:43 +0000

    removing settings creation from migration

--- db/migrate/20151115130535_create_table_settings.rb
+++ db/migrate/20151115130535_create_table_settings.rb
@@ -7,21 +7,5 @@ class CreateTableSettings < ActiveRecord::Migration
       t.boolean :active, default: true
       t.timestamps
     end
-
-    setting = Setting.new
-    setting.name = "season_data"
-    setting.value = "2014"
-    setting.save!
-
-    setting = Setting.new
-    setting.name = "season"
-    setting.value = "2015"
-    setting.save!
-
-    setting = Setting.new
-    setting.name = "game_number"
-    setting.value = 5
-    setting.save!
-
   end
 end
